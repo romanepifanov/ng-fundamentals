93421ca3bf0ee9e534d3fcfaf05402ea1ccabd71
romanepifanov <follikk@gmail.com>
2018-07-27 12:29:56 +0300

    6-8

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,27 +1,42 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 
 @Component({
     selector: 'event-thumbnail',
     template: `
-            <div class="well hoverwell thumbnail">
+            <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
                 <h2>{{event.name}}</h2>
                 <div>Date: {{event.date}}</div>
-                <div>Time: {{event.time}}</div>
+                <div [ngStyle] = "getStartTimeStyle()" [ngSwitch] = "event.time">Time: {{event.time}}
+                    <span *ngSwitchCase = "'8:00 am'">(Early Start)</span>
+                    <span *ngSwitchCase = "'10:00 am'">(Late Start)</span>
+                    <span *ngSwitchDefault>(Normal Start)</span>
+                </div>
                 <div>Price: \${{event.price}}</div>
-                <div>
-                    <span>Lacotion: {{event.location.address}}</span>
+                <div *ngIf="event.location">
+                    <span>Lacotion: {{event.location?.address}}</span>
                     <span class="pad-left">
-                        {{event.location.city}}, {{event.location.country}}
+                        {{event.location?.city}}, {{event.location?.country}}
                     </span>
+                </div>
+                <div *ngIf="event.onlineUrl">
+                    Online URL: {{event.onlineUrl}}
                 </div>
             </div>
             `,
     styles: [`
-            .pad-left{ margin-left: 10px; }
+            .thumbnail { height: 210px; }
+            .pad-left { margin-left: 10px; }
             .well div { color: #bbb; }
         `]
 })
 export class EventThumbnailComponent {
     @Input() event: any;
+
+    getStartTimeStyle(): any {
+        if(this.event && this.event.time === '8:00 am')
+            return { color: '#003300', 'font-weight': 'bold' }
+        return {}
+        
+    }
 }
